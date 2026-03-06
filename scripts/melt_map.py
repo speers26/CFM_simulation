8f0b63ff7485c22d830a426a9ac3a767605cce3c
speers26 <mattspeers33@gmail.com>
2026-03-06 14:22:54 +0000

    RACMO melt class

--- scripts/melt_map.py
+++ scripts/melt_map.py
@@ -5,8 +5,21 @@ Usage:
 
 """
 
-from force.melt import MeltMar
+from force.melt import MeltMAR, MeltRACMO
+import yaml
+
+with open("config.yaml", "r") as f:
+    config = yaml.safe_load(f)
 
 if __name__ == "__main__":
-    melt_mar = MeltMar()
-    avg_yearly_melt_mar = melt_mar.get_melt_map()
+
+    if config["rcm_name"] == "MAR":
+        melt = MeltMAR()
+    elif config["rcm_name"] == "RACMO":
+        melt = MeltRACMO()
+    else:
+        raise ValueError(
+            f"Invalid RCM name in config: {config['rcm_name']}. Must be 'MAR' or 'RACMO'."
+        )
+
+    melt.get_melt_map()
--- src/force/melt.py
+++ src/force/melt.py
@@ -5,20 +5,20 @@ import logging
 import xarray as xr
 import yaml
 
-from .process import ProcessMAR
+from .process import ProcessMAR, ProcessRACMO
 
 logging.basicConfig(level=logging.INFO)
 with open("config.yaml", "r") as f:
     config = yaml.safe_load(f)
 
 
-class MeltMar(ProcessMAR):
+class MeltMAR(ProcessMAR):
     """Class to find the average yearly total melt across the AIS for MAR
 
     Inherits from ProcessMar so that it can use the _read_data method.
     """
 
-    def __init__(self, borehole_lat: float = 0.0, borehole_lon: float = 0.0):
+    def __init__(self, borehole_lat: float = 0.0, borehole_lon: float = 0.0) -> None:
         """Initialise with generic borehole_lat and borehole_lon. This class calculates melt over entire AIS, so doesn't need borehole location.
 
         Args:
@@ -31,7 +31,7 @@ class MeltMar(ProcessMAR):
             f"{config['CFM_data_path']}/melt_maps/mar_avg_yearly_melt_map.nc"
         )
 
-    def get_melt_map(self):
+    def get_melt_map(self) -> None:
         """Get the average yearly total melt across the AIS for MAR. This method will load in the MAR data for the entire AIS, calculate the yearly sums of melt, and then average these yearly sums to get a single spatial map of how 'wet' firn is across the AIS.
 
         Returns:
@@ -51,7 +51,7 @@ class MeltMar(ProcessMAR):
 
         # Calculate the yearly sums of melt
         mar_ds["year"] = mar_ds["TIME"].dt.year
-        yearly_melt = mar_ds.groupby("year").sum(dim="TIME")["ME"]
+        yearly_melt = mar_ds["ME"].groupby("year").sum(dim="TIME")
 
         # Average these yearly sums to get a single spatial map of how 'wet' firn is across the AIS
         avg_yearly_melt = yearly_melt.mean(dim="year")
@@ -61,4 +61,55 @@ class MeltMar(ProcessMAR):
         avg_yearly_melt.to_netcdf(self._save_location)
         logging.info(f"Average yearly total melt saved to {self._save_location}")
 
-        return avg_yearly_melt
+
+class MeltRACMO(ProcessRACMO):
+    """Class to find the average yearly total melt across the AIS for RACMO
+
+    Inherits from ProcessRACMO so that it can use the _read_data method.
+    """
+
+    def __init__(self, borehole_lat: float = 0.0, borehole_lon: float = 0.0) -> None:
+        """Initialise with generic borehole_lat and borehole_lon. This class calculates melt over entire AIS, so doesn't need borehole location.
+
+        Args:
+            borehole_lat (float, optional): borehole location latitude (not needed). Defaults to 0.0 as a placeholder.
+            borehole_lon (float, optional): borehole location longitude (not needed). Defaults to 0.0 as a placeholder.
+        """
+        super().__init__(borehole_lat, borehole_lon)
+
+        self._save_location: str = (
+            f"{config['CFM_data_path']}/melt_maps/racmo_avg_yearly_melt_map.nc"
+        )
+
+    def get_melt_map(self) -> None:
+        """Get the average yearly total melt across the AIS for RACMO. This method will load in the RACMO data for the entire AIS, calculate the yearly sums of melt, and then average these yearly sums to get a single spatial map of how 'wet' firn is across the AIS.
+
+        Returns:
+            xr.DataArray: A spatial map of the average yearly total melt across the AIS.
+        """
+
+        logging.info(
+            "Reading in RACMO data for the entire AIS over the entire time period..."
+        )
+        # Load the RACMO data for the entire AIS over the entire time period
+        racmo_ds = self._read_data()
+        logging.info("RACMO data loaded successfully.")
+
+        logging.info("Calculating the average yearly total melt across the AIS...")
+        # joint list of xrarray together
+        racmo_ds = xr.concat(racmo_ds, dim="time")
+
+        # Calculate the yearly sums of melt
+        racmo_ds["year"] = racmo_ds["time"].dt.year
+        yearly_melt = racmo_ds.groupby("year").sum(dim="time")["mltgl"]
+
+        # Average these yearly sums to get a single spatial map of how 'wet' firn is across the AIS
+        avg_yearly_melt = yearly_melt.mean(dim="year")
+
+        # multiply by 60*60*24 to account for the fact that original units of melt in RACMO are kg/m2/s, and we want to convert to kg/m2/day
+        avg_yearly_melt = avg_yearly_melt * 60 * 60 * 24
+        logging.info("Average yearly total melt calculated successfully.")
+
+        # save the average yearly melt map to a netcdf file
+        avg_yearly_melt.to_netcdf(self._save_location)
+        logging.info(f"Average yearly total melt saved to {self._save_location}")
